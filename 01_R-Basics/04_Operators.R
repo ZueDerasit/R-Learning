7214bb8d585572b4973a84d197c68993a9f40c7a
ZueDerasit <zuraidah.derasit@gmail.com>
2026-09-23 19:56:55 +0800

    Create 04_Operators.R

---
+++ 01_R-Basics/04_Operators.R
@@ -0,0 +1,25 @@
+# R Basics: Operators
+
+# -------------------------
+# Arithmetic Operators
+# -------------------------
+
+x <- 10
+y <- 3
+
+x + y
+x - y
+x * y
+x / y
+x ^ y
+
+# -------------------------
+# Comparison Operators
+# -------------------------
+
+x > y
+x < y
+x >= y
+x <= y
+x == y
+x != y
