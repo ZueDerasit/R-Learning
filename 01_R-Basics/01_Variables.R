e1fbb08df68020354c85b1b9ad0fe9652e272dea
ZueDerasit <zuraidah.derasit@gmail.com>
2026-09-23 19:48:31 +0800

    Create  01_Variables.R

---
+++ 01_R-Basics/01_Variables.R
@@ -0,0 +1,17 @@
+# R Basics: Variables
+
+# Creating variables
+name <- "Zue"
+age <- 40
+height <- 153
+
+# Displaying variables
+name
+age
+height
+
+# Checking the data type
+class(name)
+class(age)
+class(height)
+
