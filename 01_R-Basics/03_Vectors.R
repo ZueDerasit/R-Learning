0b7178e8dff839518e5c0c863f7a1f4e6cc70bc8
ZueDerasit <zuraidah.derasit@gmail.com>
2026-09-23 19:55:53 +0800

    Create 03_Vectors.R

---
+++ 01_R-Basics/03_Vectors.R
@@ -0,0 +1,22 @@
+# R Basics: Vectors
+
+# Creating a numeric vector
+ages <- c(20, 25, 30, 35, 25)
+
+# Creating a character vector
+names <- c("Ali", "Siti", "John", "Mei", "Maria")
+
+# Display the vectors
+ages
+names
+
+# Check the length of a vector
+length(ages)
+length(names)
+
+# Access an element using its position
+ages[1]
+ages[3]
+
+names[2]
+names[5]
