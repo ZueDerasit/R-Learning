37571d316751aee60a57b9c1a344868b70942086
ZueDerasit <zuraidah.derasit@gmail.com>
2026-09-23 19:51:38 +0800

    Create 02_Data-Types.R

---
+++ 01_R-Basics/02_Data-Types.R
@@ -0,0 +1,19 @@
+# R Basics: Data Types
+
+# Character
+name <- "Maria"
+
+# Numeric
+height <- 159.5
+
+# Integer
+age <- 25F
+
+# Logical
+is_lecturer <- TRUE
+
+# Check the data type
+class(name)
+class(height)
+class(age)
+class(is_lecturer)
