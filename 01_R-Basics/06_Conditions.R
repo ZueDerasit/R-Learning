7df0376cea59bbfce83c70ac3af7ba0eb68fa030
ZueDerasit <zuraidah.derasit@gmail.com>
2026-09-24 19:06:03 +0800

    Create 06_Conditions.R

---
+++ 01_R-Basics/06_Conditions.R
@@ -0,0 +1,25 @@
+# R Basics: Conditions
+
+# Create a score
+score <- 75
+
+# If statement
+if (score >= 50) {
+  print("Pass")
+}
+
+# If...else statement
+if (score >= 50) {
+  print("Pass")
+} else {
+  print("Fail")
+}
+
+# If...else if...else statement
+if (score >= 80) {
+  print("Excellent")
+} else if (score >= 50) {
+  print("Pass")
+} else {
+  print("Fail")
+}
