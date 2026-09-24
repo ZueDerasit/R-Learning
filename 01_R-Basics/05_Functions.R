9b0e90c5774f137a602b664fb6b552c9f02e5874
ZueDerasit <zuraidah.derasit@gmail.com>
2026-09-24 19:04:25 +0800

    Create 05_Functions.R

---
+++ 01_R-Basics/05_Functions.R
@@ -0,0 +1,19 @@
+# R Basics: Functions
+
+# Create a vector
+scores <- c(70, 75, 80, 85, 90)
+
+# Calculate the mean
+mean(scores)
+
+# Calculate the sum
+sum(scores)
+
+# Count the number of values
+length(scores)
+
+# Find the minimum value
+min(scores)
+
+# Find the maximum value
+max(scores)
